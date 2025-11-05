7ef8e8ea5061e729109ad17353fe600bdc4ebf3c
Kostya <kostyantyn.karimov@gmail.com>
2025-11-05 15:35:38 +0200

    backend init

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    env: {
+        node: true,
+        es2020: true,
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        'prettier/prettier': 'warn',
+    },
+};
